a81c23e3e0e4d90aa2036263d2b602e9603b4c5e
ItsyBitsyGuy <igor.burbygin@gmail.com>
2014-12-01 15:59:26 +1000

    Rewriting code for block operations mode -_-

--- otherProgramming/BigNumbers/main.cpp
+++ otherProgramming/BigNumbers/main.cpp
@@ -4,14 +4,18 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <sstream>
 
 using namespace std;
+
 
 struct bigNumber
 {
     int dotAt;
-    vector<short int> integer;
+    vector<int> integer;
+    vector<int> real;
 };
+
 
 string clear(string* s)
 {
@@ -24,118 +28,61 @@ string clear(string* s)
 }
 
 
-void fillBigNumber(string* s, bigNumber* N)
-{
-    for(int i = s->length() - 1; i >= 0; i--)
-    {
-        if(s->c_str()[i] != '.')
-        {
-            N->integer.push_back((short int)(s->c_str()[i]) - 0x30);
-        }
-        else N->dotAt = i;
-    }
-}
+void fillBigNumber(string* s, bigNumber* N) {
+    int dotPos = s->find('.');
+    int t = 0;
+    string tempString;
+    bool writeToInteger = false;
+    stringstream sstr;
+    sstr << oct;
 
-
-void doAlign(bigNumber* A, bigNumber* B)
-{
-    //integer parts firstly
-    int addToA = 0;
-    int addToB = 0;
-
-    if (A->dotAt != B->dotAt)
-    {
-        B->integer.insert(B->integer.end(), A->dotAt - B->dotAt, (short int)0);
-        addToB = A->dotAt - B->dotAt;
+    if(dotPos % 4 != 0) {
+        s->insert(0, 4 - dotPos % 4, '0');
+        dotPos += 4 - dotPos % 4;
     }
 
-    if(A->integer.size() - A->dotAt != B->integer.size() + addToB - B->dotAt - addToB)
-    {
-        if(A->integer.size() - A->dotAt < B->integer.size() - B->dotAt - addToB)
-        {
-            A->integer.insert(A->integer.begin(), (B->integer.size() - B->dotAt - addToB) - (A->integer.size() - A->dotAt), (short int)0);
+    if(s->length() - dotPos - 1 % 4 != 0) {
+        int t = (s->length() - dotPos - 1);
+        for(int i = 0; i < 4 - t % 4; i++) {
+            s->push_back('0');
         }
+    }
+    for(int i = s->length(); i > 0; i -= 4) {
+        tempString = s->substr(i - 4, 4);
+        if(tempString[3] == '.') {
+            i += 3;
+            writeToInteger = true;
+            continue;
+        }
+        sstr << tempString;
+        sstr >> oct >> t;
+        sstr.clear();
+        if(writeToInteger)
+            N->integer.push_back(t);
         else
-        {
-            B->integer.insert(B->integer.begin(), (A->integer.size() - A->dotAt) - (B->integer.size() - addToB - B->dotAt), (short int)0);
-        }
+            N->real.push_back(t);
     }
 }
 
 
-void sum(bigNumber* A, bigNumber* B, string* sResult)
-{
-    bigNumber* result = new bigNumber;
-    result->integer.insert(result->integer.begin(), A->integer.size() + 1, 0);
-    short int t = 0;
-
-    for(int i = 0; i < A->integer.size(); i++)result->integer[i] = A->integer[i];
-
-    for(int i = 0; i < A->integer.size(); i++)
-    {
-        result->integer[i] += B->integer[i];
-        result->integer[i+1] += result->integer[i] / 8;
-        result->integer[i] %= 8;
-        //result->integer[i+1] += result->integer[i] / 10;
-        //result->integer[i] %= 10;
-    }
-    result->dotAt = A->dotAt + 1;
-
-    for(int i = result->integer.size() - 1; i >= 0; i--)
-    {
-        if(i == result->integer.size() - 1 - result->dotAt) sResult->push_back('.');
-        sResult->push_back(result->integer[i] + 0x30);
-    }
-
-}
-
-
-void diffirence(bigNumber* A, bigNumber* B, string* sResult)
-{
-    bigNumber* result = new bigNumber;
-    result->integer.insert(result->integer.begin(), A->integer.size(), 0);
-    for(int i = 0; i < A->integer.size(); i++) result->integer[i] = A->integer[i];
-
-    for(int i = 0; i < A->integer.size(); i++)
-    {
-        result->integer[i] = result->integer[i] - B->integer[i];
-        if(result->integer[i] < 0) {
-            //result->integer[i] += 10;
-            result->integer[i] += 8;
-            result->integer[i+1] -= 1;
-        }
-    }
-    result->dotAt = A->dotAt;
-
-    for(int i = result->integer.size() - 1; i >= 0; i--)
-    {
-        if(i == result->integer.size() - 1 - result->dotAt) sResult->push_back('.');
-        sResult->push_back(result->integer[i] + 0x30);
-    }
-}
-
-int compare(bigNumber* A, bigNumber* B)
-{
+int compare(bigNumber* A, bigNumber* B) {
     if(A->dotAt > B->dotAt) return 2;
     if(A->dotAt < B->dotAt) return 0;
     //integer parts are equal length
-    for(int i = A->integer.size() - 1; i >= 0; i--)
-    {
+    for(int i = A->integer.size() - 1; i >= 0; i--) {
         if(A->integer[i] > B->integer[i]) return 2;
         if(A->integer[i] < B->integer[i]) return 0;
     }
     return 1;
 }
 
+
 bool sanitize(string* s)
 {
     bool dotDetected = false;
-    for(int i = 0; i < s->length(); i++)
-    {
-        if(s->c_str()[i] > '7' or s->c_str()[i] < '0')
-        {
-            if(s->c_str()[i] == '.')
-            {
+    for(int i = 0; i < s->length(); i++) {
+        if(s->c_str()[i] > '7' or s->c_str()[i] < '0') {
+            if(s->c_str()[i] == '.') {
                 if(dotDetected) return false;
                 dotDetected = true;
                 if(i+1 == s->length()) return false;
@@ -150,6 +97,27 @@ bool sanitize(string* s)
     return true;
 }
 
+
+void doAlign(bigNumber* A, bigNumber* B)
+{
+    //integer
+    if(A->integer.size() < B->integer.size()) {
+        for(int i = 0; i < B->integer.size() - A->integer.size(); i++)
+            A->integer.push_back(0);
+    }
+    else if (A->integer.size() < B->integer.size()) {
+        for(int i = 0; i < A->integer.size() - B->integer.size(); i++)
+            B->integer.push_back(0);
+    }
+    //real
+    if(A->real.size() < B->real.size()) {
+        A->real.insert(A->real.begin(), B->real.size() - A->real.size(), 0);
+    }
+    else if (B->real.size() < A->real.size()) {
+        B->real.insert(B->real.begin(), A->real.size() - B->real.size(), 0);
+    }
+}
+
 int main()
 {
     string sA, sB, sSum, sDiff;
@@ -158,8 +126,7 @@ int main()
     bigNumber *T;
 
     ifstream in("in.txt");
-    if (in.is_open())
-    {
+    if (in.is_open()) {
         getline(in, sA);
         getline(in, sB);
     }
@@ -172,31 +139,30 @@ int main()
     sA = clear(&sA);
     sB = clear(&sB);
 
-    cout << sA << " || " << sB << '\n';
-
-    if(sA.compare(sB) == 0) cout << "The numbers are equal, by the way\n";
-
     fillBigNumber(&sA, A);
     fillBigNumber(&sB, B);
 
     doAlign(A, B);
 
-    if(compare(A, B) == 0)
-    {
+    for(int i = A->integer.size() - 1; i >= 0; i--)
+        cout << oct << A->integer[i];
+    cout << '.';
+    for(int i = A->real.size() - 1; i >= 0; i--)
+        cout << oct <<  A->real[i];
+    cout << '\n';
+
+
+    //cout << sA << " || " << sB << '\n';
+
+    if(sA.compare(sB) == 0) cout << "The numbers are equal, by the way\n";
+
+
+    if(compare(A, B) == 0) {
         cout << "A is less than B!\n";
         T = A;
         A = B;
         B = T;
     }
-
-    sum(A, B, &sSum);
-
-    diffirence(A, B, &sDiff);
-
-    sSum = clear(&sSum);
-    sDiff = clear(&sDiff);
-
-    cout << '\n' << sSum << " : " << sDiff << '\n';
 
     return 0;
 }
